8c5cbbede76a0d3b0f7730465ad43c8e3197956f
Patrick Jentsch <patrickjentsch@gmx.net>
2023-02-13 17:02:51 +0100

    Let the encode and decode methods behave the same as the ones from Hashids package.

--- flask_hashids.py
+++ flask_hashids.py
@@ -1,7 +1,7 @@
-from flask import abort, current_app, Flask
+from flask import current_app, Flask
 from hashids import Hashids as _Hashids
-from typing import Any, Dict
-from werkzeug.routing import BaseConverter
+from typing import Any, Dict, Tuple, Union
+from werkzeug.routing import BaseConverter, ValidationError
 
 
 class HashidMixin:
@@ -17,38 +17,45 @@ class HashidMixin:
     __id_attribute__ and must be of type int!
     '''
 
-    __id_attribute__ = 'id'
+    __id_attribute__: str = 'id'
 
     @property
     def hashid(self) -> str:
-        id = getattr(self, self.__class__.__id_attribute__)
+        id: int = getattr(self, self.__class__.__id_attribute__)
         return current_app.extensions['hashids'].encode(id)
 
 
 class HashidConverter(BaseConverter):
-    '''
-    Hashid Converter.
+    ''' Hashid Converter.
 
-    Converts given hashids from routes to integers.
-    Example:
+    Converts and decodes a hashid from routes.
+    Examples:
         @app.route('/resources/<hashid:resource_id')
         def get_resource(resource_id: int):
             print(isinstance(resource_id, int))  # True
 
-    Converts integers to hashids when generating urls.
-    Example:
+        @app.route('/resources/<hashid:resource_ids')
+        def get_resources(resource_ids: Tuple[int, ...]):
+            print(isinstance(resource_ids, tuple))  # True
+            print(all(isinstance(i, int) for i in resource_ids))  # True
+
+    Converts and encodes values when generating urls.
+    Examples:
         url_for('get_resource', resource_id=123)  # /resources/Mj3
+
+        url_for('get_resource', resource_id=(123, 456))  # /resources/Nk4
     '''
 
-    def to_python(self, value: str) -> int:
-        try:
-            decoded_value = current_app.extensions['hashids'].decode(value)
-        except IndexError:
-            abort(404)
-        return decoded_value
+    def to_python(self, hashid: str) -> Union[int, Tuple[int, ...]]:
+        decoded_hashid = current_app.extensions['hashids'].decode(hashid)
+        if isinstance(decoded_hashid, tuple) and len(decoded_hashid) == 0:
+            raise ValidationError()
+        return decoded_hashid
 
-    def to_url(self, value: int) -> str:
-        return current_app.extensions['hashids'].encode(value)
+    def to_url(self, value_or_values: Union[int, Tuple[int, ...]]) -> str:
+        if isinstance(value_or_values, int):
+            return current_app.extensions['hashids'].encode(value_or_values)
+        return current_app.extensions['hashids'].encode(*value_or_values)
 
 
 class Hashids:
@@ -75,10 +82,19 @@ class Hashids:
         app.extensions['hashids'] = self
         app.url_map.converters['hashid'] = HashidConverter
 
-    def decode(self, value: str) -> int:
-        ''' Decode a hashid to an integer. '''
-        return self._hashids.decode(value)[0]
+    def decode(self, hashid: str) -> Union[int, Tuple[int, ...], Tuple[()]]:
+        ''' Decode the passed `hashid`.
+        
+        Possible return values:
+            - `int` if the hashid contains only one value.
+            - `Tuple[int, ...]` if the hashid contains multiple values.
+            - `Tuple[()]` if the hashid is invalid.
+        '''
+        decoded_hashid = self._hashids.decode(hashid)
+        if len(decoded_hashid) == 1:
+            return decoded_hashid[0]
+        return decoded_hashid
 
-    def encode(self, value: int) -> str:
-        ''' Encode an integer to a hashid. '''
-        return self._hashids.encode(value)
+    def encode(self, *values: int) -> str:
+        ''' Encode the passed `values`. '''
+        return self._hashids.encode(*values)
